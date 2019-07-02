6ff1084302956246b33005bc96345baab0b52f10
Colin Cason <colinjcason@gmail.com>
2019-07-01 22:45:05 -0700

    created variables and for loop

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// create variables for user and computer choices
+var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z"]
+var wins = 0
+var losses = 0
+var guessesLeft = 10
+
+// an event is triggered when the user presses a key
+for(var i = 0; i < userGuess.length; i++) {
+    
+}
+
+
