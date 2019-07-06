15c249aaf676b096f151891cac78585d2c1829dc
Colin Cason <colinjcason@gmail.com>
2019-07-06 08:43:28 -0700

    final working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,13 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var randChoice = Math.floor(Math.random() * computerChoices.length);
 var computerGuess = computerChoices[randChoice];
 console.log(computerGuess);
+
+// create a reset game variable so the game resets when the user wins or loses
 var resetGame = function() {
     guessesLeft = 10;
     randChoice = Math.floor(Math.random() * computerChoices.length); 
     computerGuess = computerChoices[randChoice];
-    console.log("GAME WAS RESET");
+    alert("GAME WAS RESET");
     guessSoFar = [];
 }
 
@@ -22,25 +24,31 @@ document.onkeyup = function(event) {
     userGuess = event.key.toLowerCase();
     guessSoFar.push(userGuess);
     guessesLeft--;
+    document.getElementById("guessedSoFar").innerHTML = guessSoFar;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
-
-    // var computerGuess = computerChoices[Math.floor(math.random()) * computerChoices.length];
-
+// a function that compares user guess to computer choice and logs result
     if(userGuess === computerGuess) { 
-        console.log('you win');
+        alert('YOU GUESSED RIGHT!');
         wins++;
         resetGame();
+        document.getElementById("wins").innerHTML = wins;
     }
     else if(guessesLeft <= 0) {
         losses++;
-        console.log('you lose');
+        alert('Bad Luck!');
         resetGame();
+        document.getElementById("losses").innerHTML = losses;
     }
     console.log("wins " + wins);
-    console.log("losse " + losses);
-    console.log("Guess Left " + guessesLeft);
+    console.log("losses " + losses);
+    console.log("Guesses Left " + guessesLeft);
     console.log("Guessed So Far " + guessSoFar);
-}
+    }
+
+
+
+
 
 
 
