968461e4f67f8203199142ee7522743299a6f44b
Colin Cason <colinjcason@gmail.com>
2019-07-13 17:52:24 -0700

    fixed

---
+++ assets/javascript/game2.js
@@ -0,0 +1,18 @@
+// declare variables
+var userGuess = "";
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessedSoFar = [];
+
+// declare a variable for the computer's random choice
+var randChoice = Math.floor(Math.random() * computerChoices);
+console.log(computerChoices);
+
+// declare reset game variable that resets guesses left and letter guessed so far
+
+// create function that generates random computer choice
+for(var i = 0; i < computerChoices.length; i++) {
+
+}
